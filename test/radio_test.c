1ea8fe523ae6a6b79d55c67ca32f15cc3e8b6fcb
Gilbok Lee <gilbok.lee@samsung.com>
2016-03-02 19:30:04 +0900

    Fixed Svace issue(WGID:31189)
    Change-Id: I8ffa7eda739ae152dc262649cf6ce1ea9835a96a
    Signed-off-by: Gilbok Lee <gilbok.lee@samsung.com>

--- test/radio_test.c
+++ test/radio_test.c
@@ -320,7 +320,7 @@ int __menu(void)
 
 	if (scanf("%d", &menu_item) == 0) {
 		char temp[_MAX_INPUT_STRING_];
-		if (scanf("%s", temp) == 0)
+		if (scanf("%100s", temp) == 0)
 			printf("Error while flushing the input buffer - but lets continue\n");
 		return -1;
 	}
